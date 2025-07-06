32d0356b23c8a662d775cfe2aa750d77b52ce172
ILHAM GHAZI BIN ISKANDAR AFIAN <ilhamghazi@hopexito.com>
2025-07-06 21:06:22 +0800

    Speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <Toaster position="top-center" />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
